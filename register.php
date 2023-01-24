93e98a3ce89e350c1ffbf7964e85fa971d4c5280
lolpopk <123517188+lolpopk@users.noreply.github.com>
2023-01-25 01:13:30 +0200

    Update register.php

--- register.php
+++ register.php
@@ -3,9 +3,5 @@ $IDCode = $_REQUEST['city'];
 
 header("content-type: text/html; charset=utf-8");
 
-if($IDCode === "1"){
-    echo "<img src='image/prize.jpg' height='300' align = 'left' hspace = '10'>";
-    }
-else{
-echo "<h1>не тот("; }
+echo "<p>helo";
 ?>
