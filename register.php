86a36b2c28a05fed0af101a9f033f68ed9b1c22f
lolpopk <123517188+lolpopk@users.noreply.github.com>
2023-01-25 10:41:56 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,28 @@
-<?
-
+<?php
+print_r($_GET);
 header("content-type: text/html; charset=utf-8");
 
-echo "<p>helo";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Веди секретное слово)</title>
+</head>
+<body>
+<h1 align="center">Секретное слово))</h1>
+<div id="reg_form">
+  <form action="/" method="get">
+    <select name="city">
+           <option value="1">рыбалка</option>
+          <option value="2">дискус</option>
+          <option value="3">риск</option>
+          <option value="4">ранимый</option>
+    </select>
+      <input type="submit" value="отправть">
+  </form>
+
+</div>
+
+</body>
+</html>
